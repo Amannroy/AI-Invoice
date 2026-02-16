a4cb7a787179443fff1bf9e64ff0442ca2e24f71
Aman Roy <royaman56456@gmail.com>
2026-02-16 19:22:03 +0530

    Updated code

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
+import Features from '../components/Features';
+import Pricing from '../components/Pricing';
+import Footer from '../components/Footer';
+
 
 const Home = () => {
   return (
@@ -8,7 +12,12 @@ const Home = () => {
         <Navbar/>
         <main className="">
           <Hero />
+          <div className=" ">
+             <Features />
+          </div>
+          <Pricing />
         </main>
+        <Footer/>
     </div>
   )
 }
